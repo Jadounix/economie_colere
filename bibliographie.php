4292d0a4b7f331c3f950b684ea174dfb0c7acc2b
Jadounix <50082586+Jadounix@users.noreply.github.com>
2019-04-29 08:39:52 +0200

    Ajout de la bibliographie

---
+++ bibliographie.php
@@ -0,0 +1,59 @@
+<?php include 'entete.php';?>
+
+<div class="container">
+
+  <main class="post-content">
+    <h1>Bibliographie</h1>
+    <p>
+      <ol>
+      <li>Ansel, Dominique. « Incertitude et intensité émotionnelle en situation de négociation ». Negociations n° 13, no 1 (16 août 2010): 23‑41.
+      </li>
+      <br>
+      <li>Cannon, Walter B. « The James-Lange Theory of Emotions: A Critical Examination and an Alternative Theory ». The American Journal of Psychology 39, no 1/4 (1927): 106‑24. https://doi.org/10.2307/1415404.</li>
+      <br>
+      <li>Chanut, Véronique, Nathalie Guibert, Jacques Rojot, et Pierre-Louis Dubois. « Les limites de la rationalité limitée ? Un essai de réflexion en sciences de gestion ». Management Avenir n° 48, no 8 (2011): 97‑117.</li>
+      <br>
+      <li>Damasio. L’erreur de Descartes : La Raison Des Émotions, 1994.</li>
+      <br>
+      <li>Daniel John Zizzo. « Anger and Economic Rationality ». http://dx.doi.org/10.1080/13501780802112841.</li>
+      <br>
+      <li>Giffard, Bénédicte, et Bernard Lechevalier. « Neurosciences et affects ». Champ psychosomatique no 41, no 1 (2006): 11‑27.</li>
+      <br>
+      <li>GILAM, Gadi. « Deconstructing Anger in the Human Brain ». http://dx.doi.org/10.1007/7854_2015_408.</li>
+      <br>
+      <li>Hoorebeke, Delphine Van. « L’émotion et la prise de décision ». Revue francaise de gestion n° 182, no 2 (16 mai 2008): 33‑44.</li>
+      <br>
+      <li>J. Rotemberg, Julio. « Minimally Acceptable Altruism and the Ultimatum Game ». Journal of Economic Behavior & Organization 66 (23 mai 2006): 457‑76. https://doi.org/10.1016/j.jebo.2006.06.008.</li>
+      <br>
+      <li>Jaak Panksepp, et Margaret R. Zellner. « Towards A Neurobiologically Based Unified Theory of Aggression ». https://studylib.net/doc/18255429/towards-a-neurobiologically-based-unified-theory-of-aggre...</li>
+      <br>
+      <li>James, William. « What is an Emotion? » Mind 9, no 34 (1884): 188‑205.</li>
+      <br>
+      <li>Kavoussi, Richard, Phyllis Armstead, et Emil Coccaro. « The neurobiology of impulsive agression ». Psychiatric Clinics of North America 20, no 2 (1 juin 1997): 395‑403. https://doi.org/10.1016/S0193-953X(05)70319-1.</li>
+      <br>
+      <li>Lotstra, Françoise. « Le cerveau émotionnel ou la neuroanatomie des émotions ». Cahiers critiques de therapie familiale et de pratiques de reseaux no 29, no 2 (2002): 73‑86.</li>
+      <br>
+      <li>Nummenmaa, Lauri, Enrico Glerean, Riitta Hari, et Jari K. Hietanen. « Bodily Maps of Emotions ». Proceedings of the National Academy of Sciences, 26 décembre 2013, 201321664. https://doi.org/10.1073/pnas.1321664111.</li>
+      <br>
+      <li>Otniel E. Dror. « The Cannon-Bard Thalamic Theory of Emotions: A Brief Genealogy and Reappraisal ». http://dx.doi.org/10.1177/1754073913494898.</li>
+      <br>
+      <li>Petit, Emmanuel. « Émotions et prise de décision dans le jeu de l’ultimatum ». Les Cahiers Internationaux de Psychologie Sociale Numéro 83, no 3 (2009): 71‑90.</li>
+      <br>
+      <li>———. « La négociation : les enseignements du jeu de l’ultimatum ». Negociations n° 13, no 1 (16 août 2010): 77‑96.</li>
+      <br>
+      <li>———. « Le rôle des affects en économie ». Revue d’economie politique Vol. 119, no 6 (2009): 859‑97.</li>
+      <br>
+      <li>Sandrine Gil. « Comment étudier les émotions en laboratoire ? ». https://www.researchgate.net/publication/266501465_Comment_etudier_les_emotions_en_laboratoire.</li>
+      <br>
+      <li>Strack, Fritz, Leonard L. Martin, et Sabine Stepper. « Inhibiting and Facilitating Conditions of the Human Smile: A Nonobtrusive Test of the Facial Feedback Hypothesis. » Journal of Personality and Social Psychology 54, no 5 (1988): 768‑77. https://doi.org/10.1037/0022-3514.54.5.768.</li>
+      <br>
+      <li>  Weisfeld, Glenn E., et Stefan M. M. Goetz. « Applying Evolutionary Thinking to the Study of Emotion ». Behavioral Sciences 3, no 3 (septembre 2013): 388‑407. https://doi.org/10.3390/bs3030388.</li>
+      </ol>
+    </p>
+
+
+  </main>
+</div>
+
+
+<?php include 'pied.php'; ?>
