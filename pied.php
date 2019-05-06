707d440759455205bb70f50e05958db63a6d2751
Jadounix <50082586+Jadounix@users.noreply.github.com>
2019-05-06 18:35:49 +0200

    Add files via upload

--- pied.php
+++ pied.php
@@ -2,6 +2,8 @@
 	<footer>
 		<div id="names">
 			<p>Created from scratch by <span class="name">Thibo Rosemplatt</span> & <span class="name">Jade Petit</span></p>
+			<br>
+			<p>Merci à notre professeur <span class="name">M. Emmanuel PETIT</span> qui a encadré la réalisation de ce TER.</p>
 		</div>
 	</footer>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
