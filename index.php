808658bbcd1ee8d6bf8af0f3d8c87b428bb6ffb0
Jadounix <50082586+Jadounix@users.noreply.github.com>
2019-05-09 13:36:18 +0200

    modification des abstracts

--- index.php
+++ index.php
@@ -2,30 +2,25 @@
 
 <div class='paper-stack'>
   <h2>ABSTRACT</h2>
-  <p>Your emotions may affect your economic behaviour more than you think. Previous studies have indeed revealed that consumption, implicit or explicit bargaining are leaded by your affects. In this study, we are focusing especially on anger. In this
-    period of yellow vest movement, it is imperative to understand how anger and economic behaviour are related.</p>
-
-  <p>To this end, we first did a literature review. Then we made up an experiment, by replicating “the ultimatum game” as a web-game.
-    This game deals with implicit bargaining between two people, and “take it or leave it” decisions. Our goal was to prove that most of participants played with a rational behaviour. For this purpose, we established and tested two hypotheses on the
-    behavior of the participants, along the lines of the theory of rationality.
-    It has been played by 68 participants, mainly L3 Miashs students. </p>
-
-  <p>Results refuted both of our hypothesis. Participants behaviour was shown not to be that rational, but the results were still interesting as they brought new elements, qualifying the understanding of the game.
-    The limits of the experiment, mainly originated in its background, were also involved in the non-validation of our hypotheses.</p>
+  <p>Your emotions may affect your economic behaviour more than you think. Previous studies have indeed revealed that consumption and implicit or explicit bargaining are affected by sentiments. In this study, we focus particularly on anger. In the context of the “yellow vest” movement, it is imperative to understand how anger and economic behaviour are related. </p>
+  <p>To this end, we first reviewed the literature. Then we created an experiment, by replicating “the ultimatum game” as a web application.
+  This game deals with implicit bargaining between two people,  and “take it or leave it” decisions. Our goal was to prove that most of the players exhibed rational behaviour. For this purpose, we established and tested two hypotheses on the behaviour of the participants, along the lines of the theory of rationality. This game has been played by 68 participants, mainly final year of  MIASHS* degree students. </p>
+  <p>The results refuted both of our hypotheses. Participants’ behaviour has been shown to be less rational than expected, but the results were still interesting as they revealed new elements, extending the understanding of the game.
+  The limitations of the web browser based context contributed to some extent to the unexpected results.</p>
+  <br>
+  <p>*Mathematics and computer science, applied to social and human sciences.</p>
 </div>
 
 <div class='paper-stack'>
   <h2>RÉSUMÉ</h2>
-  <p>Vos émotions pourraient affecter votre comportement économique plus que vous ne le pensez. En effet, des études antérieures ont en effet révélé que la consommation ou la négociation sont conduites par les affects. Dans le cadre de ce TER, nous nous sommes concentré particulièrement sur la colère. En cette période de mouvement des gilets jaunes, il est impératif de comprendre comment la colère et le comportement économique sont liés.</p>
+  <p>Vos émotions pourraient affecter votre comportement économique plus que vous ne le pensez. Des études antérieures ont en effet révélé que la consommation ou la négociation sont conduites par les affects. Dans le cadre de ce TER, nous nous sommes concentré particulièrement sur la colère. En cette période de mouvement des gilets jaunes, il est impératif de comprendre comment la colère et le comportement économique sont liés.</p>
 
   <p>À cette fin, nous avons d'abord procédé à une revue de la littérature. Puis nous avons réalisé une expérience, en reproduisant "le jeu de l'ultimatum" sur le site web de ce TER.
-    Ce jeu traite des négociations implicites entre deux personnes, et des décisions “à prendre ou à laisser”. Notre but était de prouver que la plupart des participants jouaient avec un comportement rationnel. Pour ce faire, nous avons établi et
-    testé deux hypothèses sur le comportement des participants, allant dans le sens de la théorie de la rationalité.
-    Il a été joué par 68 participants, principalement des étudiants de L3 Miashs.</p>
-
-  <p>Les résultats réfutent nos deux hypothèses. Le comportement des participants n'était pas si rationnel que ça, mais les résultats restent néanmoins intéressants car ils apportent de nouveaux éléments, permettant de nuancer la compréhension du
-    jeu.
-    Les limites de l'expérience, provenant principalement du contexte, ont également été impliquées dans la non-validation de nos hypothèses. </p>
+  Ce jeu traite de négociations implicites entre deux personnes, et de décisions de type “à prendre ou à laisser”. Notre but était de prouver que la plupart des participants jouaient avec un comportement rationnel. Pour ce faire, nous avons établi et testé deux hypothèses sur le comportement des participants, allant dans le sens de la théorie de la rationalité. Ce jeu a été joué par 68 participants, principalement des étudiants de L3 Miashs.
+  </p>
+  <p>Les résultats réfutent nos deux hypothèses. Le comportement des participants n'était pas si rationnel que ça, mais les résultats restent néanmoins intéressants car ils apportent de nouveaux éléments, permettant de nuancer la compréhension du jeu.
+  Les limites de l'expérience, provenant principalement du contexte, ont également été impliquées dans la non-validation de nos hypothèses.
+  </p>
 
 </div>
 
