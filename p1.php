3e98bd2b7128226c2f6c25fdecd284809f176945
Jadounix <50082586+Jadounix@users.noreply.github.com>
2019-05-09 13:46:34 +0200

    correction des fautes

--- p1.php
+++ p1.php
@@ -11,18 +11,18 @@
     <p>Il existe de nombreuses définitions de l'émotion dans les différentes disciplines qui s'intéressent à ce phénomène. En psychologie, on considère qu'elle se traduit généralement comme une réaction psychologique et physique à un stimulus. Ce
       stimulus peut être externe, ou être déclenché par ses propres états mentaux.</p>
     <p>Ce qui semble caractériser les émotions, c'est leur nature transitoire. Elles sont généralement de courte durée, même si elles peuvent se muer en quelque chose de plus long, appelée une « humeur » si la cause de l'émotion dure dans le temps.
-      Les émotions peuvent également marquer la mémoire, notamment si elles sont intenses . Elles peuvent se traduire par un comportement, par exemple un comportement agressif dans le cas de la colère.</p>
-    <p>Certaines émotions forment les émotions dites « de base ». Elle sont six d'après le neurologue Damasio (1994) : Bonheur (ou joie), tristesse, peur, colère, dégoût et surprise. Mais Plutchnik en distingue, se regroupant en 4 paires contenant à
+      Les émotions peuvent également marquer la mémoire, notamment si elles sont intenses. Elles peuvent se traduire par un comportement, par exemple un comportement agressif dans le cas de la colère.</p>
+    <p>Certaines émotions forment les émotions dites « de base ». Elle sont six d'après le neurologue Damasio (1994) : bonheur (ou joie), tristesse, peur, colère, dégoût et surprise. Mais Plutchnik en distingue, se regroupant en 4 paires contenant à
       chaque fois les deux émotions opposées : joie/tristesse, acceptation/dégoût, colère/peur, surprise/anticipation. Les autres émotions viendraient de mélange entre ces émotions de bases, et de leurs variations d'intensité.
       D'après Damasio les émotions joueraient un rôle important dans le raisonnement et la prise de décision.
     </p>
-    <p>Les émotions ont initialement été été étudiées de façon expérimentale, à cause notamment de la difficulté à les induire en laboratoire dans le cadre d'une expérience. Se pose également le problème de l'éthique : peut-on susciter des réactions
+    <p>Les émotions ont initialement été peu étudiées de façon expérimentale, à cause notamment de la difficulté à les induire en laboratoire dans le cadre d'une expérience. Se pose également le problème de l'éthique : peut-on susciter des réactions
       émotionnelles, telles que de la peur, de la colère ou encore de la tristesse dans un cadre expérimental ? À partir du 19ème siècle, des approches théoriques ont cependant été proposées.</p>
     <ul>
       <li>La théorie de James et Lange (1884) [11] :</li>
       <p>James et Lange proposent une théorie périphériste des émotions : les stimuli émotionnels engendrent des changements corporels, et c'est la perception des ces changements qui induit l'émotion. L’émotion est donc la conséquence de symptômes
         corporels, et non l’inverse. “Notre coeur bat plus vite, nous transpirons, donc nous avons peur.”
-        <br />Cette théorie peut étonner de prime abord. Pourtant des expériences ont été menées, sur lesquels les chercheurs se sont appuyés : Par exemple, tenir un crayon entre ses dents oblige à activer les zones zygomatiques, et suscite des
+        <br />Cette théorie peut étonner de prime abord. Pourtant des expériences ont été menées, sur lesquels les chercheurs se sont appuyés : par exemple, tenir un crayon entre ses dents oblige à activer les zones zygomatiques, et suscite des
         émotions positives. De la même manière, tenir un crayon entre ses lèvres active les muscles Platysma, et suscite des émotions négatives, [20].
         Néanmoins cette théorie reste contestée, et moins utilisée aujourd'hui pour décrire les émotions.
         Cette théorie est dite périphériste car on considère que ce sont les événements physiologiques périphériques qui permettent la naissance des émotions.
@@ -39,16 +39,16 @@
       <p>Schachter et Singer proposent une théorie cognitiviste des émotions. Les individus interprètent leurs réaction corporelles en fonction de leur expérience, et du contexte. D’où la possibilité d’émotions différentes pour une même réaction
         corporelle.
         <br />Une expérience célèbre illustrant cette théorie est celle du pont suspendu de Dutton et Aron (1974) :
-        Une enquêtrice fait passer un questionnaire à deux groupes d’hommes, et leur laisse son numéro de téléphone à la fin pour qu’ils puissent la recontacter pour obtenir les résultats. Le premier groupe passait le questionnaire dans un lieu
+        une enquêtrice fait passer un questionnaire à deux groupes d’hommes, et leur laisse son numéro de téléphone à la fin pour qu’ils puissent la recontacter pour obtenir les résultats. Le premier groupe passait le questionnaire dans un lieu
         neutre, tandis que le deuxième le passait sur un pont suspendu vertigineux. Les résultats montrent que les hommes qui ont passé le questionnaire sur le pont sont plus nombreux à rappeler l'enquêtrice, que ceux ayant passé le questionnaire
         dans un lieu neutre.
-        L’interprétation est la suivante : Être sur le pont suspendu active une modification physiologique chez les individus, qu’ils attribuent (artificiellement) à la présence de l'enquêtrice.
+        L’interprétation est la suivante : être sur le pont suspendu active une modification physiologique chez les individus, qu’ils attribuent (artificiellement) à la présence de l'enquêtrice.
       </p>
     </ul>
 
     <h2>Définition et importance de la rationalité économique</h2>
     <p>La rationalité économique est une hypothèse selon laquelle les individus chercheraient à maximiser la satisfaction de leurs besoins. Dans le cadre de la négociation par exemple, chaque individu cherche à maximiser son profit, et minimiser ses
-      coûts. Dans La rationalité, (Idées économiques et sociales n°165), un comportement est défini comme rationnel lorsqu'il est fondé sur une comparaison entre les moyens à la disposition de l'agent et les fins qu'il cherche à atteindre.
+      coûts. Dans La rationalité, (<i>Idées économiques et sociales n°165</i>), un comportement est défini comme rationnel lorsqu'il est fondé sur une comparaison entre les moyens à la disposition de l'agent et les fins qu'il cherche à atteindre.
       <br />D'après l'article de Zizzo, Anger and economic rationality [5], il y a deux sortes de rationalité économique : une rationalité déontologique et une rationalité conséquentialiste. Lorsqu’elle est définie selon le principe déontologique, la
       rationalité des choix et des actions d’un individu est évaluée en fonction des règles, des principes, qui ont été suivis pour atteindre un objectif. Par opposition, lorsque la rationalité est définie selon le principe conséquentialiste, alors
       elle est évaluée en fonction des conséquences des actions d’un individu, en se souciant davantage des objectifs que des actions qui ont été menées pour les atteindre.
