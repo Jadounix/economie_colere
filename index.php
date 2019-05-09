81971356a92254a8dbce90a29ba36ae18eeaed42
Jadounix <50082586+Jadounix@users.noreply.github.com>
2019-05-09 14:33:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,12 @@
   <p>Vos émotions pourraient affecter votre comportement économique plus que vous ne le pensez. Des études antérieures ont en effet révélé que la consommation ou la négociation sont conduites par les affects. Dans le cadre de ce TER, nous nous sommes concentré particulièrement sur la colère. En cette période de mouvement des gilets jaunes, il est impératif de comprendre comment la colère et le comportement économique sont liés.</p>
 
   <p>À cette fin, nous avons d'abord procédé à une revue de la littérature. Puis nous avons réalisé une expérience, en reproduisant "le jeu de l'ultimatum" sur le site web de ce TER.
-  Ce jeu traite de négociations implicites entre deux personnes, et de décisions de type “à prendre ou à laisser”. Notre but était de prouver que la plupart des participants jouaient avec un comportement rationnel. Pour ce faire, nous avons établi et testé deux hypothèses sur le comportement des participants, allant dans le sens de la théorie de la rationalité. Ce jeu a été joué par 68 participants, principalement des étudiants de L3 Miashs.
+  Ce jeu traite de négociations implicites entre deux personnes, et de décisions de type “à prendre ou à laisser”. Notre but était de prouver que la plupart des participants jouaient avec un comportement rationnel. Pour ce faire, nous avons établi et testé deux hypothèses sur le comportement des participants, allant dans le sens de la théorie de la rationalité. Ce jeu a été joué par 68 participants, principalement des étudiants de L3 MIASHS*.
   </p>
   <p>Les résultats réfutent nos deux hypothèses. Le comportement des participants était moins rationnel que prévu, mais les résultats restent néanmoins intéressants car ils apportent de nouveaux éléments, permettant de nuancer la compréhension du jeu.
   Les limites de l'expérience, provenant principalement de sa modélisation sous forme d'un jeu vidéo, ont également été impliquées dans la non-validation de nos hypothèses.
   </p>
+  <p>*Mathématiques et Informatique Appliquées aux Sciences Humaines et Sociales.</p>
 
 </div>
 
