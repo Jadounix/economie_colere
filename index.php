b3219820d561bd27821c699f4a8e88996360ab72
Jadounix <50082586+Jadounix@users.noreply.github.com>
2019-05-09 14:29:11 +0200

    corrections

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
   <p>À cette fin, nous avons d'abord procédé à une revue de la littérature. Puis nous avons réalisé une expérience, en reproduisant "le jeu de l'ultimatum" sur le site web de ce TER.
   Ce jeu traite de négociations implicites entre deux personnes, et de décisions de type “à prendre ou à laisser”. Notre but était de prouver que la plupart des participants jouaient avec un comportement rationnel. Pour ce faire, nous avons établi et testé deux hypothèses sur le comportement des participants, allant dans le sens de la théorie de la rationalité. Ce jeu a été joué par 68 participants, principalement des étudiants de L3 Miashs.
   </p>
-  <p>Les résultats réfutent nos deux hypothèses. Le comportement des participants n'était pas si rationnel que ça, mais les résultats restent néanmoins intéressants car ils apportent de nouveaux éléments, permettant de nuancer la compréhension du jeu.
-  Les limites de l'expérience, provenant principalement du contexte, ont également été impliquées dans la non-validation de nos hypothèses.
+  <p>Les résultats réfutent nos deux hypothèses. Le comportement des participants était moins rationnel que prévu, mais les résultats restent néanmoins intéressants car ils apportent de nouveaux éléments, permettant de nuancer la compréhension du jeu.
+  Les limites de l'expérience, provenant principalement de sa modélisation sous forme d'un jeu vidéo, ont également été impliquées dans la non-validation de nos hypothèses.
   </p>
 
 </div>
